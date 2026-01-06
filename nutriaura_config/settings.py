09498ad0c5eaac9bd266f951b2a7d8b00c2058cb
Yashwanth A <yash2706.git@gmail.com>
2026-01-06 12:23:56 +0000

    configure settings to look for templates and static files in project's root directry

--- nutriaura_config/settings.py
+++ nutriaura_config/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'nutriaura_config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,4 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
